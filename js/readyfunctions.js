59048205ff8f5ae9a8b1f0369be8976b5cbce049
Hasnain Afzal <Huss@Hasnains-MacBook-Air.local>
2014-07-27 14:53:24 +1000

    First commit

---
+++ js/readyfunctions.js
@@ -0,0 +1,132 @@
+$(document).ready(function() {
+
+    $('#tableload').load('views/Table.html');
+
+    $('#loginbtn').on('click', $.validate({
+        form: '#loginuser',
+        modules: 'security',
+        onError: function() {
+            alert('Validation failed');
+        },
+        onSuccess: function() {
+            return submitloginForm();
+        }
+    }));
+
+    function submitloginForm() {
+        $.ajax({
+            type: 'POST',
+            url: 'https://domytaxreturn.com.au/expenseapp/GAE/login.php',
+            data: $('#loginuser').serialize(),
+            success: function(response) {
+                localStorage.setItem("UID", response);
+                var x = localStorage.getItem("UID");
+                if (x == 'User not found') {
+
+                    alert('User not found, please register');
+
+                } else if (x == "Incorrect password!") {
+                    alert("Check Username or Email");
+                } else {
+                    $('.form_result').append('Logged in with UID:' + x + ' <a id = "logout">Logout</a>');
+                    $('#owl-homeslider').hide();
+                    $('#start').show();
+                    window.location.href = window.location.href;
+                }
+            }
+        });
+        return false;
+    }
+    $('#registerbtn').on('click', $.validate({
+        form: '#registeruser',
+        modules: 'security',
+        onError: function() {
+            alert('Validation failed');
+        },
+        onSuccess: function() {
+            return submitregisterForm();
+        }
+    }));
+
+    function submitregisterForm() {
+        $.ajax({
+            type: 'POST',
+            url: 'https://domytaxreturn.com.au/expenseapp/GAE/register.php',
+            data: $('#registeruser').serialize(),
+            success: function(response) {
+                localStorage.setItem("UID", response);
+                var x = localStorage.getItem("UID");
+                if (x == 'User Already Registered, please login') {
+
+                    alert('User Already Registered, please login');
+                } else if (x == "Couldn't register user, please try again'") {
+                    alert("Couldn't register user, please try again'");
+                } else {
+                    $('.form_result').append('Logged in with UID:' + x + ' <a id = "logout">Logout</a>');
+                    $('#owl-homeslider').hide();
+                    $('#start').show();
+                    window.location.href = window.location.href;
+                }
+            }
+        });
+        return false;
+    }
+
+})
+
+
+$(document).ready(function() {
+
+    $("#owl-homeslider").owlCarousel({
+
+        slideSpeed: 1000,
+        paginationSpeed: 1000,
+        singleItem: true
+    });
+
+
+});
+
+$(document).on("swipeleft", ".transaction-summary-info", function(event) {
+    var listitem = $(this),
+        // These are the classnames used for the CSS transition
+        dir = event.type === "swipeleft" ? "left" : "right",
+        // Check if the browser supports the transform (3D) CSS transition
+        transition = $.support.cssTransform3d ? dir : false;
+    Hello();
+});
+
+function Hello() {
+
+    alert('left');
+}
+
+$(document).on("swiperight", ".transaction-summary-info", function(event) {
+    var listitem = $(this),
+        // These are the classnames used for the CSS transition
+        dir = event.type === "swipeleft" ? "left" : "right",
+        // Check if the browser supports the transform (3D) CSS transition
+        transition = $.support.cssTransform3d ? dir : false;
+    right();
+});
+
+function right() {
+
+    alert('right');
+}
+
+$(document).ready(function() {
+    var x = localStorage.getItem("UID");
+    if (x == null) {} else {
+
+        $('.form_result').append('Logged in with UID:' + x + ' <a id = "logout">Logout</a>');
+        $('#owl-homeslider').hide();
+        $('#start').show();
+    }
+    $('#logout').on('click', function() {
+        localStorage.removeItem("UID");
+        window.location.href = window.location.href;
+    });
+
+
+})
