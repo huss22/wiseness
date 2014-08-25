fd15122eae154ad4414f02a43f5990ec0530629b
Hasnain Afzal <huss22@users.noreply.github.com>
2014-08-26 09:39:31 +1000

    Update readyfunctions.js

--- js/readyfunctions.js
+++ js/readyfunctions.js
@@ -149,3 +149,18 @@ function pinman() {
     }
 
 }
+
+var x=document.getElementById("demo");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.watchPosition(showPosition);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+function showPosition(position)
+  {
+  x.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;	
+  }
